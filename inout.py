c05b0e6b9aa3b90e8fc8afbf859940ffb3580031
sriraksha <psriraksha22@gmail.com>
2025-08-09 21:08:50 +0530

    Delete inout.py

--- inout.py
+++
@@ -1,7 +0,0 @@
-boy_name=input("Boy Name: ")
-boyAge=int(input("age: "))
-girl_name=input("girl Name: ")
-GirlAge=int(input("age: "))
-diff=abs(boyAge-GirlAge) #absolute 
-
-print(f"{boy_name} Loves {girl_name} .Age difference {diff}")
