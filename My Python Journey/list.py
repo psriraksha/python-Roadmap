8ab4a924081687ab891acc33fe836b974336b87f
sriraksha <psriraksha22@gmail.com>
2025-08-12 22:27:25 +0530

    Add files via upload

---
+++ My Python Journey/list.py
@@ -0,0 +1,21 @@
+print("**Lists operation**")
+item=["bread","butter","milk","egg","jam"]
+
+#add
+print("given list: " +str(item))
+append=item.insert(1,"coffe")#2nd pos
+append=item.append("cake")#at end
+print(f"after appending item at end and second postion: {item}")
+
+#delete
+remove=item.pop(1)
+print(f"after removing 2nd item from list: {item}")
+print("*******************************************")
+list=[10,90,60,40,30,29]
+print("given list: "+ str(list))
+list.sort()#sorting
+list.reverse()
+print(f"Descending order: {list}")
+list.reverse()#reverse
+print(f"reversed list: {list}")
+
