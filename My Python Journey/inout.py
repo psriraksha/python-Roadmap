ba66b485ec0459797cf790ca34b5b4210843fced
sriraksha <psriraksha22@gmail.com>
2025-08-13 22:08:59 +0530

    Update inout.py

--- My Python Journey/inout.py
+++ My Python Journey/inout.py
@@ -1,7 +1,8 @@
-boy_name=input("Boy Name: ")
+boy_name=input("Boy Name: ") #input
 boyAge=int(input("age: "))
 girl_name=input("girl Name: ")
 GirlAge=int(input("age: "))
 diff=abs(boyAge-GirlAge) #absolute 
 
-print(f"{boy_name} Loves {girl_name} .Age difference {diff}")
+
+print(f"{boy_name} Loves {girl_name} .Age difference {diff}") #display
